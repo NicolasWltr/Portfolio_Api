d10f4948834d01716a0c437635fd0b3d5043ba5a
NicolasWltr <nicolas-walter1@gmx.de>
2025-04-10 19:25:37 +0200

    CORS + GlobalPrefix

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,22 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.setGlobalPrefix('/api');
+
+  const allowedOrigins = ['http://localhost:3000', 'https://walternicolas.de', 'https://www.walternicolas.de'];
+
+  app.enableCors({
+    origin: (origin, callback) => {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true); // Allow request
+      } else {
+        callback(new Error('Not allowed by CORS')); // Reject request
+      }
+    },
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    credentials: true,
+  });
+
   await app.listen(process.env.PORT ?? 3001, '0.0.0.0');
 }
 bootstrap();
