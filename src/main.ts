1952dca33d9bfeb9784838589c7d4190f1c69128
NicolasWltr <nicolas-walter1@gmx.de>
2025-05-05 20:50:14 +0200

    Added CORS origin

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,14 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('/api');
-  app.enableCors();
+  app.enableCors({
+    origin: [
+      'https://walternicolas.de',
+      'https://www.walternicolas.de',
+      'http://localhost:3000',
+      'http://127.0.0.1:3000'
+    ]
+  });
   await app.listen(process.env.PORT ?? 3001, '0.0.0.0');
 }
 bootstrap();
